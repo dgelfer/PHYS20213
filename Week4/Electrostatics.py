b8cde12f69b2b23060c5c062e8032d9d34d997df
Dan Gelfer <dangelfer@pcp047443pcs.pcv.reshall.calpoly.edu>
2013-04-29 17:05:22 -0700

    finished notes

--- Week4/Electrostatics.py
+++ Week4/Electrostatics.py
@@ -1,14 +1,14 @@
 import numpy as np
 
 def pointPotential(x,y,q,posx,posy): 
-  k = 9e9
-  Vxy = ((k*q)/np.sqrt((x-posx)**2)+(y-posy)**2)
-  return Vxy
+    k = 9.e9
+    Vxy = ((k*q)/np.sqrt( (x-posx)**2 +(y-posy)**2))
+    return Vxy
   
 
 
 def dipolePotential(x,y,q,d): # finds dipole potential at a certain distance d
-    k = 9e9
-    Vxy_2 = (k*q/(x**2+(y-(d/2))**2)**(1/2.)) - (k*q/(x**2+(y+(d/2))**2)**(1/2.))
+    k = 9.e9
+    Vxy_2 = (k*q/(y**2+(x-(d/2))**2)**(1/2.)) - (k*q/(y**2+(x+(d/2))**2)**(1/2.))
     return Vxy_2
 
